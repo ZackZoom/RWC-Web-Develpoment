560aabab6de13a7b66d469db416f168b4dcd6c16
ZackZoom <johnakin16@gmail.com>
2023-04-15 12:33:00 -0400

    new me

--- Project/js/about_me.js
+++ Project/js/about_me.js
@@ -4,9 +4,9 @@ const Ayomide =
     age: "15",
     school: "mount olive high school",
     grade: "9",
-    favorite_Subject: "math",
-    image_url: "https://media.discordapp.net/attachments/1074073669552324698/1094263360704483348/Snapchat-224888250.jpg",
-    site: "file:///C:/Users/Owner/Documents/GitHub/RWC-Web-Develpoment/Project/index.html",
+    favorite_subject: "math",
+    image_url: "https://cdn.discordapp.com/attachments/760443337978544128/1096835482370965605/Snapchat-2139727942.jpg",
+    home_page_url: "file:///C:/Users/Owner/Documents/GitHub/RWC-Web-Develpoment/Project/index.html",
     about_me:
     "Hello there! My name is Ayomide John and I am 15 years old. I am a freshman in Mount Olive High school. I Joined RWC when I was in 8th grade. I live my with dad,mom and 2 younger siblings"
 }
